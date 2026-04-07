c821d2ee433477830c17f943bd410ab482230de1
Kitty311620 <Terryanne.ndululu@gmail.com>
2026-04-07 09:43:55 +0300

    Add fuel level checks to if.js

---
+++ if.js
@@ -0,0 +1,29 @@
+let cratecolor = "Silver"
+
+if (cratecolor == "Gold")
+{
+    console.log("You found a Legendary Sword!")
+}
+else if (cratecolor == "Silver")
+{
+    console.log("You found a Med-Kit!")
+}
+else 
+{
+ console.log("It's just a box of stale pepperoni. Yuck.")
+}
+//-------------------------------------------------------------------
+//Fuel test
+let fuellevel = 45
+if (fuellevel > 80)
+{
+console.log("Full tank!Let's drive to the Secret Base")
+}
+else if(fuellevel > 20 )
+{
+    console.log("Running low. Stop at the gas station")
+}
+else
+{
+    console.log("Empty! Grab your running shoes!")
+}
