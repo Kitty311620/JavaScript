387901953c12c881c1526bc14f7b8208b8116882
Kitty311620 <Terryanne.ndululu@gmail.com>
2026-04-07 09:24:47 +0300

    Add score grading logic in If.js

---
+++ If.js
@@ -0,0 +1,18 @@
+let score = 50
+
+if (score > 90)
+{
+     console.log("You got a A* ")
+}
+else if (score > 80)
+{
+     console.log("You got a plain A")
+}
+else if (score > 70)
+{
+     console.log("You got a B*")
+}
+else
+{
+    console.log("Try harder next time") 
+}
