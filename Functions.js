08c7079cb97d57b6bdf4dc52a6e3bd3dbc67d6a6
Kitty311620 <Terryanne.ndululu@gmail.com>
2026-04-08 09:18:27 +0300

    Add Intro and Leaving functions with console messages

---
+++ Functions.js
@@ -0,0 +1,13 @@
+//function is just a group of code.
+//method is the alternative of method.
+function Intro()
+{
+console.log("Welcome to my Treehouse!")
+console.log("Would you like some tea or coffee?")
+}
+Intro()
+function Leaving()
+{
+    console.log("Thank you for coming. Bye!")
+}
+Leaving()
